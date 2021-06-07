f730ec2f6e37eb8d40ff9e88c88e57e8cd422e40
Anup Sukumaran <sukumar@qaptive.co.in>
2021-06-07 08:14:01 +0100

    Initial Commit

---
+++ Pickup App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pickup App
+//
+//  Created by Sukumar Anup Sukumaran on 07/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
